2391625a2495fb85691896cf59068b81b5bf1f30
DanielCuyuch07 <danielproyectos13@gmail.com>
2024-07-07 00:36:52 -0600

    [UPDATE] Se agregaron nuevos campos para validacione del formulario episodio_106

---
+++ ManejoPresupuesto/Controllers/TiposCuentasController.cs
@@ -0,0 +1,26 @@
+﻿using ManejoPresupuesto.Models;
+using Microsoft.AspNetCore.Mvc;
+
+namespace ManejoPresupuesto.Controllers
+{
+    public class TiposCuentasController : Controller
+    {
+        public IActionResult Crear()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public IActionResult Crear(TipoCuenta tipoCuenta)
+        {
+            // Si el modelo no es valido (returnamos a la vista para mensajes de error)
+            if (!ModelState.IsValid)
+            {
+                return View(tipoCuenta);
+            }
+
+            return View();
+        }
+
+    }
+}
---
+++ ManejoPresupuesto/Models/TipoCuenta.cs
@@ -0,0 +1,30 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace ManejoPresupuesto.Models
+{
+    public class TipoCuenta
+    {
+        public int Id { get; set; }
+        [Required (ErrorMessage = "el campo {0} es requerido ")]
+        public string Nombre { get; set; }
+        public int UsuarioId { get; set; }
+        public int Orden { get; set; }
+
+
+        /* Campos de pruebas */
+
+        [Required (ErrorMessage = " El campo {0} es requerido ")]
+        [EmailAddress(ErrorMessage = "El campo debe ser un correo electronico valido")]
+        public string Email { get; set; }
+
+        [Range(minimum:18, maximum:130, ErrorMessage = "El valor debe estar entre {1} y {2}")]
+        public int Edad { get; set; }
+
+        [Url(ErrorMessage = "El campo debe ser una URL valida")]
+        public string URL { get; set; }
+
+        [CreditCard(ErrorMessage = "La tarjeta de credito no es valida")]
+        public string TarjetaCredito { get; set; }
+
+    }
+}
